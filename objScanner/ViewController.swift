7d55da33beedbec52cc7b3a4f6c1047ef99ae3e7
Andres Rojas <andres.rojas@endava.com>
2019-02-25 16:07:44 -0500

    Create new struct for groups

--- objScanner/ViewController.swift
+++ objScanner/ViewController.swift
@@ -11,52 +11,8 @@ import SceneKit
 
 class ViewController: UIViewController {
 
-    //MARK: - Properties
-    lazy var roofMaterial: SCNMaterial = {
-        let material = SCNMaterial()
-        let image = roofTextures[0]
-        material.multiply.contentsTransform =  SCNMatrix4MakeScale(1/10, 1 / 10, 1)
-        material.multiply.contents = image
-        material.multiply.wrapS = .repeat
-        material.multiply.wrapT = .repeat
-        material.multiply.minificationFilter = .none
-        material.multiply.mipFilter = .none
-        material.isDoubleSided = false
-        material.lightingModel = .lambert
-
-        return material
-    }()
-
-    lazy var wallMaterial: SCNMaterial = {
-        let material = SCNMaterial()
-        let image = wallTextures[0]
-        material.multiply.contentsTransform =  SCNMatrix4MakeScale(1/10, 1 / 10, 1)
-        material.multiply.contents = image
-        material.multiply.wrapS = .repeat
-        material.multiply.wrapT = .repeat
-        material.multiply.minificationFilter = .none
-        material.multiply.mipFilter = .none
-        material.isDoubleSided = false
-        material.lightingModel = .lambert
-
-        return material
-    }()
-
-    lazy var windowMaterial: SCNMaterial = {
-        let material = SCNMaterial()
-        let image = #imageLiteral(resourceName: "window")
-        material.multiply.contentsTransform =  SCNMatrix4MakeScale(1/10, 1 / 10, 1)
-        material.multiply.contents = image
-        material.multiply.wrapS = .repeat
-        material.multiply.wrapT = .repeat
-        material.multiply.minificationFilter = .none
-        material.multiply.mipFilter = .none
-        material.isDoubleSided = false
-        material.lightingModel = .lambert
-
-        return material
-    }()
-
+    // MARK: - Properties
+    var evObjects = [EVObject]()
     let wallTextures = [#imageLiteral(resourceName: "wall1"), #imageLiteral(resourceName: "wall2"), #imageLiteral(resourceName: "wall3")]
     let roofTextures = [#imageLiteral(resourceName: "roof1"), #imageLiteral(resourceName: "roof2"), #imageLiteral(resourceName: "roof3")]
 
@@ -66,14 +22,21 @@ class ViewController: UIViewController {
     // MARK: - IBActions
 
     @IBAction func setWallsTexture(_ sender: UIButton) {
+        let objectIndex = Int.random(in: 0 ..< evObjects.count)
+        var rootNode = evObjects[objectIndex]
+        guard !rootNode.name.contains("Roof"), !rootNode.name.contains("WallPen") else { return }
         let index = Int.random(in: 0 ..< 3)
         let texture = wallTextures[index]
-        wallMaterial.multiply.contents = texture
+        rootNode.material.multiply.contents = texture
     }
+    
     @IBAction func setRoofsTexture(_ sender: UIButton) {
+        let objectIndex = Int.random(in: 0 ..< evObjects.count)
+        var rootNode = evObjects[objectIndex]
+        guard rootNode.name.contains("Roof") else { return }
         let index = Int.random(in: 0 ..< 3)
         let texture = roofTextures[index]
-        roofMaterial.multiply.contents = texture
+        rootNode.material.multiply.contents = texture
     }
 
     // AMRK: - Properties
@@ -140,38 +103,16 @@ extension ViewController {
 
 
         for group in groups {
-            for face in group.faces {
-                let indices: [Int32] = [0, 1, 2]
-
-                var vertices = [SCNVector3]()
-                for vertex in face.vertices {
-                    let vector = SCNVector3(vertex.x, vertex.y, vertex.z)
-                    vertices.append(vector)
-                }
-
-                let sVertices = SCNGeometrySource(vertices: vertices)
-                let normals = face.normals.filter({$0 != nil }) as! [SCNVector3]
-                let sNormals = SCNGeometrySource(normals: normals)
-                let textures = face.textures.filter({$0 != nil}) as! [CGPoint]
-                let sTextures = SCNGeometrySource(textureCoordinates: textures)
-
-                let element = SCNGeometryElement(indices: indices, primitiveType: .triangles)
-
-                let geometry = SCNGeometry(sources: [sVertices, sNormals, sTextures], elements: [element])
-
-                let node = SCNNode(geometry: geometry)
-                node.name = group.name
-
-                if group.name.contains("Roof") {
-                    geometry.materials = [roofMaterial]
-                } else if group.name.contains("WallPen") {
-                    geometry.materials = [windowMaterial]
-                } else {
-                    geometry.materials = [wallMaterial]
-                }
-
-                scene.rootNode.addChildNode(node)
+            var type: NodeType = .wall
+            if group.name.contains("Roof") {
+                type = .roof
+            } else if group.name.contains("WallPen") {
+                type = .penetration
             }
+            let evObject = EVObject(type: type, group: group)
+            self.evObjects.append(evObject)
+
+            scene.rootNode.addChildNode(evObject.rootNode)
         }
 
     }
@@ -184,5 +125,68 @@ extension ViewController {
 
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
         scene.rootNode.addChildNode(cameraNode)
+    }
+}
+
+enum NodeType {
+    case roof, wall, penetration
+}
+
+struct EVObject {
+    let name: String
+    let rootNode = SCNNode()
+    let type: NodeType
+
+    lazy var material: SCNMaterial = {
+        let material = SCNMaterial()
+        material.multiply.contentsTransform =  SCNMatrix4MakeScale(1/10, 1 / 10, 1)
+        material.multiply.wrapS = .repeat
+        material.multiply.wrapT = .repeat
+        material.multiply.minificationFilter = .none
+        material.multiply.mipFilter = .none
+        material.isDoubleSided = false
+        material.lightingModel = .lambert
+
+        return material
+    }()
+
+    init(type: NodeType, group: Group ) {
+        self.name = group.name
+        self.type = type
+
+        switch type {
+        case .roof:
+            material.multiply.contents = #imageLiteral(resourceName: "roof1")
+        case .wall:
+            material.multiply.contents = #imageLiteral(resourceName: "wall1")
+        default:
+            material.multiply.contents = #imageLiteral(resourceName: "window")
+        }
+
+
+        for face in group.faces {
+            let indices: [Int32] = [0, 1, 2]
+
+            var vertices = [SCNVector3]()
+            for vertex in face.vertices {
+                let vector = SCNVector3(vertex.x, vertex.y, vertex.z)
+                vertices.append(vector)
+            }
+
+            let sVertices = SCNGeometrySource(vertices: vertices)
+            let normals = face.normals.filter({$0 != nil }) as! [SCNVector3]
+            let sNormals = SCNGeometrySource(normals: normals)
+            let textures = face.textures.filter({$0 != nil}) as! [CGPoint]
+            let sTextures = SCNGeometrySource(textureCoordinates: textures)
+
+            let element = SCNGeometryElement(indices: indices, primitiveType: .triangles)
+
+            let geometry = SCNGeometry(sources: [sVertices, sNormals, sTextures], elements: [element])
+
+            let node = SCNNode(geometry: geometry)
+            node.geometry?.materials = [material]
+            rootNode.addChildNode(node)
+
+        }
     }
 }
